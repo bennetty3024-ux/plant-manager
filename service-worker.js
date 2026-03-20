cd67de9efd2afef337699f81901680c0c82ff9d0
bennetty3024-ux <bennetty3024@gmail.com>
2026-03-20 09:05:38 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,7 @@
-const CACHE="yoonseul-pro";
-
-self.addEventListener("install",e=>{
-
-e.waitUntil(
-
-caches.open(CACHE).then(cache=>{
-
-return cache.addAll([
-
-"./",
-"./index.html",
-"./style.css",
-"./app.js"
-
-])
-
+self.addEventListener('install',e=>{
+self.skipWaiting()
 })
 
-)
-
-});
-
-self.addEventListener("fetch",e=>{
-
-e.respondWith(
-
-caches.match(e.request).then(r=>r||fetch(e.request))
-
-)
-
-});
+self.addEventListener('fetch',event=>{
+event.respondWith(fetch(event.request))
+})

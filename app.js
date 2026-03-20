1ab9b33f3b34d09bf84062f5d0762db88911965d
bennetty3024-ux <bennetty3024@gmail.com>
2026-03-20 09:16:15 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,95 +1,32 @@
-let plants = JSON.parse(localStorage.getItem("plants") || "[]")
+let plants = JSON.parse(localStorage.getItem("plants") || "[]");
 
-function save(){
-localStorage.setItem("plants", JSON.stringify(plants))
+function render(){
+const list=document.getElementById("plantList");
+list.innerHTML="";
+plants.forEach((p,i)=>{
+const li=document.createElement("li");
+li.innerHTML=p+" <button onclick='removePlant("+i+")'>삭제</button>";
+list.appendChild(li);
+});
 }
 
 function addPlant(){
-
-let category=document.getElementById("category").value
-let group=document.getElementById("group").value
-let name=document.getElementById("name").value
-let pot=document.getElementById("pot").value
-let memo=document.getElementById("memo").value
-
-let plant={
-category,
-group,
-name,
-pot,
-memo
+const name=document.getElementById("plantName").value;
+if(!name) return;
+plants.push(name);
+localStorage.setItem("plants",JSON.stringify(plants));
+document.getElementById("plantName").value="";
+render();
 }
 
-plants.push(plant)
-
-save()
-renderPlants()
-
+function removePlant(i){
+plants.splice(i,1);
+localStorage.setItem("plants",JSON.stringify(plants));
+render();
 }
 
-function renderPlants(){
+render();
 
-let list=document.getElementById("plantList")
-let search=document.getElementById("search").value
-
-list.innerHTML=""
-
-plants
-.filter(p=>p.name.includes(search))
-.forEach((p,i)=>{
-
-let div=document.createElement("div")
-
-div.className="plant"
-
-div.innerHTML=`
-<b>${p.name}</b><br>
-대분류: ${p.category}<br>
-중분류: ${p.group}<br>
-화분: ${p.pot}<br>
-메모: ${p.memo}
-<button onclick="deletePlant(${i})">삭제</button>
-`
-
-list.appendChild(div)
-
-})
-
+if("serviceWorker" in navigator){
+navigator.serviceWorker.register("service-worker.js");
 }
-
-function deletePlant(i){
-
-plants.splice(i,1)
-
-save()
-
-renderPlants()
-
-}
-
-
-function calcSoil(){
-
-let size=parseFloat(document.getElementById("soilSize").value)
-
-let soil=size*0.6
-let sand=size*0.3
-let charcoal=size*0.1
-
-document.getElementById("soilResult").innerHTML=`
-실내용흙 ${soil} L<br>
-산야초 ${sand} L<br>
-훈탄 ${charcoal} L
-`
-
-}
-
-function calcFertilizer(){
-
-let size=parseFloat(document.getElementById("fertSize").value)
-
-document.getElementById("fertResult").innerHTML=`${size} 알`
-}
-
-
-renderPlants()
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,12 @@
-self.addEventListener('install',e=>{
-self.skipWaiting()
-})
+self.addEventListener('install',function(event){
+console.log('Service Worker installed');
+self.skipWaiting();
+});
 
-self.addEventListener('fetch',event=>{
-event.respondWith(fetch(event.request))
-})
+self.addEventListener('activate',function(event){
+console.log('Service Worker activated');
+});
+
+self.addEventListener('fetch',function(event){
+event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
+});

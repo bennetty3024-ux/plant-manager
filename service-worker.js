26a89d12815804cb867d82bf576421a93cd5ba53
bennetty3024-ux <bennetty3024@gmail.com>
2026-03-20 09:24:12 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,7 @@
-self.addEventListener('install',function(event){
-console.log('Service Worker installed');
-self.skipWaiting();
-});
-
-self.addEventListener('activate',function(event){
-console.log('Service Worker activated');
-});
+self.addEventListener('install',function(e){
+self.skipWaiting()
+})
 
 self.addEventListener('fetch',function(event){
-event.respondWith(fetch(event.request).catch(()=>caches.match(event.request)));
-});
+event.respondWith(fetch(event.request))
+})
